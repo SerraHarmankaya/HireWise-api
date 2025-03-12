f6ff9e61e63b1e50485a5447841ad7faaccd8402
sharmankaya <serraharmankayaa@gmail.com>
2025-03-12 09:29:36 +0300

    secured

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,8 +3,7 @@ const User = require('../models/user')
 
 const auth = async (req ,res ,next) => {
 
-    // token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzQ1ZDFhYzgzOTc1N2U1NmZiOGUxZjEiLCJpYXQiOjE3MzI3NDU3MTd9.qj8cpmY-Xkp_Sm0ZR67avbGtR3jTQbDh-IHp5TLaSzU ' 
-    // token is something like (the standard way) that but we do not want the baerer part when we authenticate the user. So for that we need to split the token int the above line.
+    
 
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
